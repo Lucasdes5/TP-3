117392d4fdf27b3d13be861f356d82089c4b70d9
Your Name <you@example.com>
2024-01-15 22:39:14 -0300

    trabajo practico

---
+++ clase12/javascript.1/app.js
@@ -0,0 +1,32 @@
+
+//esto es un comentario
+
+alert('Bienvenido a mi pagina')
+
+//tipos de dato: cadenas de textos(string) , numros booleanos( verdadero o  falso (true fals))
+var nombre ='lucas'; //(string)-cadena de texto , siempre entre comillas
+var numero = 42;
+
+
+console.log(nombre);
+console.log(numero);
+
+nombre = 'javier';
+console.log(nombre)
+
+
+//suma
+var suma = 50+50;
+console.log(suma);
+
+//multiplicacion
+var multiplicacion =2 * 24;
+console.log(multiplicacion)
+
+
+//concatenacion ( poder unificar todas las variables en una sola linea ( frase))
+var nombre = 'Lucas';
+var apellido ='Pizarro';
+var edad = 31; 
+
+console.log(``)
